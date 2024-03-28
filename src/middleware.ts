e844c25aa1bc74428f561a8b4ef5f838984ae458
RRTDKS <ryan.alam001@gmail.com>
2024-03-28 19:48:45 +0200

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/', '/dashboard'], // An array of paths
+  publicRoutes: ['/'], // An array of paths
 });
  
 export const config = {
   matcher: [
     "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
     "/(api|trpc)(.*)"
   ]
 };
