a26fd8fcaa0ae310b7eb6c38e39527fa38d4c43b
Assif Alam <asif.k.alam@net.usek.edu.lb>
2024-07-11 13:52:15 +0300

    Update editcoachesandactivities.tsx

--- src/app/components/admin/editcoachesandactivities.tsx
+++ src/app/components/admin/editcoachesandactivities.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { FaPlus, FaEdit, FaTrash, FaUserFriends, FaUser } from 'react-icons/fa'
-import FileUploadDropzone from './FileUploadDropZone'
+import FileUploadDropzone from './FileUploadDropzone'
 import { RingLoader } from 'react-spinners'
 import {
 	addCoach,
