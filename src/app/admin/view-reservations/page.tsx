b395dfeee2a47084f39874d323e9b7e0e4f6d252
Assif Alam <asif.k.alam@net.usek.edu.lb>
2024-07-12 12:11:59 +0300

    admin panel design change done

--- src/app/admin/view-reservations/page.tsx
+++ src/app/admin/view-reservations/page.tsx
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import CoachesandActivitiesAdminPage from "../../components/admin/editcoachesandactivities"
-import NavbarComponent from '@/app/components/users/navbar';
-import AdminNavbarComponent from '@/app/components/admin/adminnavbar';
-import { checkRoleAdmin } from '../../../../utils/roles';
-import { redirect } from 'next/navigation';
-import ViewReservationsComponent from '@/app/components/admin/viewreservations';
+import React, { useState, useEffect } from 'react'
+import CoachesandActivitiesAdminPage from '../../components/admin/editcoachesandactivities'
+import NavbarComponent from '@/app/components/users/navbar'
+import AdminNavbarComponent from '@/app/components/admin/adminnavbar'
+import { checkRoleAdmin } from '../../../../utils/roles'
+import { redirect } from 'next/navigation'
+import ViewReservationsComponent from '@/app/components/admin/viewreservations'
 
 export default function viewreservations() {
-  if (!checkRoleAdmin("admin")) {
-    redirect("/");
-  }
-  return(
-    <div>
-      <AdminNavbarComponent/>
-      <section className='mt-5'>
-      <ViewReservationsComponent/>
-      </section>
-      <section className="container mt-5 mx-auto px-4 sm:px-6 lg:px-8">
-
-      </section>
-
-    </div>
-
-  )
-
+	if (!checkRoleAdmin('admin')) {
+		redirect('/')
+	}
+	return (
+		<div className='bg-gray-900'>
+			<AdminNavbarComponent />
+			<section className=''>
+				<ViewReservationsComponent />
+			</section>
+		</div>
+	)
 }
