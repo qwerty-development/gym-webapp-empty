10fdc00fe471c2833bda0452869e07b0f0ab4582
RRTDKS <ryan.alam001@gmail.com>
2024-07-22 15:05:21 +0300

    Accoiunting dashboard + transaxtions

--- src/app/admin/accounting/dashboard/page.tsx
+++ src/app/admin/accounting/dashboard/page.tsx
@@ -1,9 +1,14 @@
+import AdminNavbarComponent from '@/app/components/admin/adminnavbar'
 import React from 'react'
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard page</div>
-  )
+    return (
+        <div>
+            <AdminNavbarComponent />
+            <div className='bg-gray-800 min-h-screen'>Dashboard  page</div>
+
+        </div>
+    )
 }
 
 export default Dashboard;
--- src/app/admin/accounting/transactions/page.tsx
+++ src/app/admin/accounting/transactions/page.tsx
@@ -1,9 +1,14 @@
+import AdminNavbarComponent from '@/app/components/admin/adminnavbar';
 import React from 'react'
 
 const Transactions = () => {
-  return (
-    <div>page</div>
-  )
+    return (
+        <div>
+            <AdminNavbarComponent />
+            <div className='bg-gray-800 min-h-screen'>Transaction  page</div>
+
+        </div>
+    )
 }
 
 export default Transactions;
