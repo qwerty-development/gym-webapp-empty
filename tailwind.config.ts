89ee9313f343da79d4c50defce887c775e0e249d
Assif Alam <asif.k.alam@net.usek.edu.lb>
2024-08-01 12:49:44 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,25 +10,28 @@ const config: Config = {
 		extend: {
 			colors: {
 				green: {
-					200: '#c3d1bb',
-					300: '#a5b89a',
-					400: '#36783a',
-					500: '#4c6f46',
-					600: '#446340',
-					700: '#3c583a',
-					800: '#344d33',
-					900: '#2c422c'
+					200: '#FFB3A7', // Lighter shade of bittersweet
+					300: '#FF9281', // Light shade of bittersweet
+					400: '#FF715B', // Primary bittersweet
+					500: '#2274A5', // Primary blue
+					600: '#1D6089', // Darker shade of blue
+					700: '#18506E', // Even darker shade of blue
+					800: '#134054', // Much darker shade of blue
+					900: '#0E303A' // Very dark shade of blue
 				},
 				gray: {
-					200: '#d1d4d1',
-					300: '#b8bcb8',
-					400: '#9fa39f',
-					500: '#868a86',
-					600: '#6d726d',
-					700: '#454c45', // primary color
-					800: '#353b35',
-					900: '#232623'
-				}
+					200: '#1B252F', // Darker shade of primary black
+					300: '#010B13', // Primary black
+					400: '#0E1821', // Slightly lighter shade of primary black
+					500: '#28323D', // Light shade of primary black
+					600: '#D1D5D9', // Darker shade of primary white
+					700: '#E2E5E8', // Dark shade of primary white
+					800: '#F2F3F4', // Primary white
+					900: '#F7F8F9' // Slightly lighter shade of primary white
+				},
+				// Adding default color overrides
+				white: '#FF7D6A', // Dark blue-gray shade
+				black: '#010B13' // Same as primary black
 			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
